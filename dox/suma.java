6c1c015fc7a50b07f52f5930560b864cfd9f23a8
ww971 <74652551+ww971@users.noreply.github.com>
2020-11-30 10:50:19 +0100

    Add files via upload

---
+++ dox/suma.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class suma {
+
+  public static void main(String[] args) {
+    double suma = 0;
+    int tablica[] = new int[5];
+    for (int i = 0; i < tablica.length; i++) {
+      String tekst = JOptionPane.showInputDialog("podaj watosc pola tablicy");
+      int liczba = Integer.parseInt(tekst);
+      tablica[i] = liczba;
+      suma = tablica[i] + suma;
+    }
+    System.out.println("Suma liczb z tablicy wynosi " + suma);
+  }
+}
